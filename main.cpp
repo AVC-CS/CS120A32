fc46aa222fbb372a654b805252a44ec9a7c255ae
Kyu Lee <kwooler@gmail.com>
2024-08-13 18:54:41 -0700

    A32

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 int main()
 {
-  int numFemale;
-  int numMale;
-  double percF, percM;
+  int numFemale, numMale, numOthers;
+  double percF, percM, percO;
 
+  cout << "Enter the number of students: Male, Female and Others";
+  cin >> numMale >> numFemale >> numOthers;
   /* ************************************
     Code your program here
   ************************************ */
@@ -14,4 +15,5 @@ int main()
   cout << setprecision(2) << fixed;
   cout << "Percentage of Male: " << percF << endl;
   cout << "Percentage of Female: " << percM << endl;
+  cout << "Percentage of Others: " << percO << endl;
 }
